6bd76681c8d33a273c164087d39794995cea0a9f
Gor90 <hovsepian@yandex.ru>
2016-02-10 07:41:51 -0800

    Hi Commit

--- Tasks/Calculator/Gor Hovsepyan/Calculate/Program.cs
+++ Tasks/Calculator/Gor Hovsepyan/Calculate/Program.cs
@@ -14,7 +14,7 @@ namespace Calculate
         [STAThread]
         static void Main()
         {  
-              ///
+              ///Hi
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Calculator());
