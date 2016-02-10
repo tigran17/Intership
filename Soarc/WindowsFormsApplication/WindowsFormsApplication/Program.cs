ab4da616c691ceef5bc3c677ad2d40247125e24e
Gor Rustamyan <gor.rustamyan@gmail.com>
2016-02-10 19:35:37 +0400

    Added TEst file

--- Soarc/WindowsFormsApplication/WindowsFormsApplication/Program.cs
+++ Soarc/WindowsFormsApplication/WindowsFormsApplication/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApplication
         [STAThread]
         static void Main()
         {
-              
+               
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
