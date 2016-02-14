32b5f0f3572cd5e9e24b90222a4ef43e6dd66d7a
hrach99 <hrachya_khachatryan@edu.aua.am>
2016-02-14 22:50:05 +0400

    Implementation of the ReadBytes function

---
+++ Tasks/Messenger/Hrach Khachatryan/Intership.Messenger.Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intership.Messenger.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var server = new Server();
+            if (args != null && args.Length > 0)
+            {
+                server.Config.Port = Convert.ToInt32(args[0]);//first arg is port
+
+                if (args.Length > 1)
+                    server.Config.MaxClients = Convert.ToInt32(args[1]);//second arg is max client
+
+
+            }
+
+
+            server.Start();
+
+            Console.ReadLine();
+
+        }
+    }
+}
